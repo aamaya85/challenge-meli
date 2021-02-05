a5496a6c7696798b473e4413b42cb5b89866da70
AAmaya <aamaya@campus.ungs.edu.ar>
2021-02-05 05:05:07 -0300

    - Se agrega el archivo app.yaml para el deploy en Google App engine </br> . Se agrega la función getMessages para reconstruir el mensaje original

--- api/controllers/data-controller.js
+++ api/controllers/data-controller.js
@@ -1,3 +1,5 @@
+const _ = require('underscore')
+
 module.exports = {
   getLocation: (req, res) => {
     const distances = req.body.satellites.map((sat) => sat.distance)
@@ -9,9 +11,38 @@ module.exports = {
     } else {
       res.status(404).send('Error in request')
     }
+  },
+  getMessage: (req, res) => {
+    
+    const message = getMessage(_.pluck(req.body.satellites, 'message'));
+      res.status(200).send({
+        message
+      })
+    
   }
 }
 
+
+const getMessage = (messages) => {
+
+  messages = _.zip(... messages) // Agrupar las distintas palabras de los mensajes en orden de llegada
+  messages = messages.map((message) => _.compact(message)) // Remover palabras vacías
+  messages = _.flatten(messages); // Unificar palabras en un array único
+  
+  let decriptedMessage = []
+
+  // Comparamos y removemos las palabras consecutivas (repetidas)
+  for(let i = 0, length = messages.length; i < length; i++){
+    if (i == 0 || messages[i-1].toString() !== messages[i].toString()) {
+      decriptedMessage.push(messages[i]);
+    }
+  }
+
+  const originalMessage = decriptedMessage.join(" ").replace(",", " ")
+
+  return originalMessage
+
+}
 
 const getLocation = (distances) => {
 
@@ -22,10 +53,18 @@ const getLocation = (distances) => {
     sato: [40, 3]
   }
 
-  const x1 = satellitesPositions.kenobi[0], y1 = satellitesPositions.kenobi[1]
-  const x2 = satellitesPositions.skywalker[0], y2 = satellitesPositions.skywalker[1]
-  const x3 = satellitesPositions.sato[0], y3 = satellitesPositions.sato[1]
-  const r1 = distances[0], r2 = distances[1], r3 = distances[2]
+  const x1 = satellitesPositions.kenobi[0]
+        y1 = satellitesPositions.kenobi[1]
+
+  const x2 = satellitesPositions.skywalker[0]
+        y2 = satellitesPositions.skywalker[1]
+
+  const x3 = satellitesPositions.sato[0]
+        y3 = satellitesPositions.sato[1]
+
+  const r1 = distances[0]
+        r2 = distances[1]
+        r3 = distances[2]
 
   A = ((-2) * x1 + 2 * x2)
   B = ((-2) * y1 + 2 * y2)
--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	port: process.env.PORT || 3000
+	port: 8080
 }
